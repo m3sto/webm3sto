0d606dbae9ae59dd65fdbc718227a09ce645159e
m3sto <m3sto.wolfly@gmail.com>
2025-01-02 21:26:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
 // config.js
 const config = "main.html"; 
 const maintenanceMode = false; 
-
-if (maintenanceMode == true) {
-    window.location.href = "https://m3sto.github.io/webm3sto/index.html";
-}
