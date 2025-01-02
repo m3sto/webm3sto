816458eb7358c4baa42a5245bc8a6d371e022693
m3sto <m3sto.wolfly@gmail.com>
2025-01-02 21:14:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const config = "main.html";
 const maintenanceMode = true; 
 
 if (maintenanceMode) {
-    window.location.href = "https://m3sto.github.io/webm3sto/bakim.html";
+    window.location.href = "https://m3sto.github.io/webm3sto/index.html";
 }
